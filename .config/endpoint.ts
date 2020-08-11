ccbef1f1f7a190f884410ca04876beabd3e1a7e1
GTSpray <69210426+GTSpray@users.noreply.github.com>
2020-08-11 19:37:05 +0200

    remove typo

--- .config/endpoint.ts
+++ .config/endpoint.ts
@@ -1,5 +1,4 @@
 export default {
   wsendpoint: "wss://xkjbd.sse.codesandbox.io",
-  backendendpoint: "https://xkjbd.sse.codesandbox.io",
-  test: ""
+  backendendpoint: "https://xkjbd.sse.codesandbox.io"
 };
